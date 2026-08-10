5c86057fdcfeda7edb3773b2d22c6bf92bf710f6
CLAUDIO SANTOS DA SILVA FILHO <claudio.ssfilho@senacsp.edu.br>
2026-08-10 11:38:39 -0300

    Quarta Aula

---
+++ ExercicioControl.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class ExercicioControl
+    {
+        ExercicioModel modelo;//Conectando a Classe Model e Control
+        private int opcao;
+
+        public ExercicioControl()
+        {
+            this.modelo = new ExercicioModel();
+            this.opcao = 0;
+            SetOpcao(0);
+
+        }//Construtor Padrão
+
+        public int GetOpcao()
+        {
+            return this.opcao;
+        }//fim do GetOpcao
+
+        public void SetOpcao(int opcao)
+        {
+            this.opcao = opcao;
+        }//fim do set
+        public void Coletar()
+        {
+            //Peguei os dois
+            Console.Write("Informe o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Vou passar para o construtor com parâmetro
+            this.modelo = new ExercicioModel(num1, num2);
+        }//fim do coletar = Pega os dois números digitados
+        // mostrar as opçoes de operação na tela
+
+        public void ColetarReal()
+        {   // pegar dois números
+            Console.WriteLine("Informe o primeiro número:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(" Informe o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            this.modelo = new ExercicioModel(num1, num2);
+        }
+        public void Menu()
+        {
+            Console.WriteLine("\n escolha uma das opções abaixo: \n" +
+                              " 0. Sair\n" +
+                              " 1. soma\n" +
+                              " 2. Subtração\n" +
+                              " 3. Divisão\n" +
+                              " 4. Multiplicação\n" +
+                              " 5. Potência\n" +
+                              " 6.Potência com Parâmetros\n" +
+                              " 7.Raiz\n" +
+                              " 8. Raiz com Parâmetros\n" +
+                              " 9. Exercicio 01\n" +
+                              " 10. Exercicio 02\n" +
+                              " 11. Exercicio 03\n" +
+                              " 12. Exercicio 04\n" +
+                              " 13. Exercicio 05\n");
+
+            SetOpcao(Convert.ToInt32(Console.ReadLine()));
+            Console.Clear();//limpa console
+        }//fim do menu
+
+
+
+        //executar o menu
+        public void ExecutarMenu()
+        {
+            do
+            {
+                Menu();//Mostrar as opções
+                switch (GetOpcao())
+                {
+                    case 0:
+                        Console.WriteLine(" Obrigado!");
+                        break;
+                    case 1:
+                        Coletar();//pegar os dois numeros
+                        Console.WriteLine(" A soma é: " + this.modelo.Somar());//Fazer a operação de soma
+                        break;// finalizar o case
+                    case 2:
+                        Coletar();
+                        Console.WriteLine(" A Subtração é: " + this.modelo.Subtrair());
+                        break;
+                    case 3:
+                        Coletar();
+                        //Validação
+                        if (this.modelo.Dividir() == -2)
+                        {
+                            Console.WriteLine(" Imposssivel Dividir por número negativo!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" A Divisão é: " + this.modelo.Dividir());
+                        }
+                        break;
+
+                    case 4:
+                        Coletar();
+                        Console.WriteLine(" A Multiplicação é: " + this.modelo.Multiplicar());
+                        break;
+                    case 5:
+                        Coletar();
+                        Console.WriteLine("A Potência é: " + this.modelo.Potencia());
+                        break;
+                    case 6:
+                        Console.WriteLine("Informe a base: ");
+                        double bas = Convert.ToDouble(Console.ReadLine());
+
+                        double expoente = Convert.ToDouble(Console.ReadLine());
+                        //Mostrar o resultado da operação
+                        Console.WriteLine("A potência é: " + this.modelo.Potencia(bas,expoente));
+                        break;
+                    default:
+                        Console.WriteLine(" Opção escolhida não é valida!");
+                        break;
+                    case 7:
+                        ColetarReal();
+                        Console.WriteLine("A raiz do primeiro número é:" + this.modelo.RaizNum3());
+                        Console.WriteLine("A raiz do segundo número é:" + this.modelo.RaizNum4());
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe o primeiro número");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo numero: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("A raiz do primeiro numero é: " + this.modelo.Raiz(num1));
+                        Console.WriteLine("A raiz do segundo numero é: " + this.modelo.Raiz(num2));
+                        break;
+                    case 9:
+                        Console.WriteLine("Informe o ano: ");
+                        int ano = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.modelo.Exercicio01(ano));
+                        break;
+                    case 10:
+                        Console.WriteLine("Informe um número:");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(this.modelo.Exercicio02(num));
+
+                    break;
+
+                    case 11:
+                        Console.WriteLine("Informe sua idade: ");
+                        int idade = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(this.modelo.Exercicio03(idade));
+                    break;
+
+                    case 12:
+                        Console.WriteLine("Informe o primeiro número do paramêtro:");
+                        int interv1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Informe o segundo número do paramêtro:");
+                        int interv2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Informe o número que deseja comparar:");
+                        int numComp = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(this.modelo.Exercicio04(interv1,interv2, numComp));
+                        break;
+                    case 13:
+                        Console.WriteLine(this.modelo.Exercicio05());
+                    break;
+
+
+
+
+
+
+                }//fim escolha
+            } while (GetOpcao() != 0); //fim do do...while
+
+
+
+
+
+
+
+
+        }//fim do método executar menu
+
+
+
+    }//fim da classe
+}//fim do projeto
---
+++ ExercicioModel.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class ExercicioModel //CONTROLA AS REGRAS
+    {
+        //Área para declaração de variáveis globais
+        private int num1;
+        private int num2;
+        private double num3;
+        private double num4;
+        private int resultado;
+
+        //Método Construtor: Instância as variáveis na memória
+        public ExercicioModel()
+        {
+            SetNum1(0);
+            SetNum2(0);
+            SetResultado(0);
+        }//fim do método construtor
+
+
+        
+        public ExercicioModel(int num1, int num2)  // contrutor de inteiro
+        {
+            SetNum1(num1);
+            SetNum2(num2);
+            SetResultado(0);
+        }//fim do construtor
+
+        public ExercicioModel(double num1, double num2) //contrutor real
+        {
+            SetNum3(num1);
+            SetNum4(num2);
+
+        }// fim do construtor
+
+        //Métodos GETS E SETS
+        //GET = Pegar, Obter, Consultar
+        //SET = Alterar, Modificar, Configurar
+        
+        
+        //publico pq vai ser acessado por todos,
+        //ele vai servir pra pessoas de fora ver as variaveis privadas
+        public int GetNum1() 
+        {
+            return this.num1;
+        }//fim do getNum1
+
+        public void SetNum1(int num1)
+        {
+            this.num1 = num1;
+        }//fim do setNum1
+
+        public int GetNum2()
+        {
+            return this.num2;
+        }//fim do getNum2
+
+        public void SetNum2(int num2)
+        {
+            this.num2 = num2;
+        }//fim do setNum2
+
+        public int GetResultado()
+        {
+            return this.resultado;
+        }//fim do método
+
+        public void SetResultado(int resultado)
+        {
+            this.resultado = resultado;
+        }//fim do método setResultado
+
+        public double GetNum3()
+        {
+            return this.num3;
+        }// fim do Getnum3
+
+        public void SetNum3(double num3)
+        {
+            this.num3 = num3;
+        }// fim do Setnum3
+
+        public double GetNum4()
+        {
+            return this.num4;
+        }// fim do Getnum4
+
+        public void SetNum4(double num4)
+        {
+            this.num4 = num4;
+        }//fim do Setnum4
+
+
+
+
+        public int Somar()
+        {
+            return this.num1 + this.num2;
+        }//fim do método somar
+
+        public int Subtrair()
+        {
+            SetResultado(GetNum1() - GetNum2());
+            return GetResultado();
+        }//fim do método subtrair
+
+        public int Multiplicar()
+        {
+            SetResultado(GetNum1() * GetNum2());
+            return GetResultado();
+        }//fim do multiplicar
+
+        public double Dividir()
+        {
+            if (GetNum2() <= 0)
+            {
+                return -2;
+            }
+            else if (GetNum2() == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                SetResultado(GetNum1() / GetNum2());
+                return GetResultado();
+            }//fim do if
+        }//fim do dividir
+
+        public double Potencia(double bas, double expoente)
+        {
+            return Math.Pow(bas, expoente);
+        }//fim da potencia
+
+        public double Raiz(double num)
+        {
+            return Math.Sqrt(num);
+        }//fim da raiz
+        public double Potencia()
+        {
+            return Math.Pow(GetNum3(), GetNum4());
+
+        }// fim do potencia parametro   
+
+        public double RaizNum3()
+        {
+            return Math.Sqrt(GetNum3());
+        }
+
+        public double RaizNum4()
+        {
+            return Math.Sqrt(GetNum4());
+        }
+
+
+
+        public string Exercicio01(int ano)// Declaração de variavel
+        {
+            if (ano % 4 == 0)
+            { return ano + " é bissexto!"; }
+            else { return ano + " não é bissexto!"; }
+        }//Fim método ano bissexto
+
+
+        public string Exercicio02(int num)
+        {
+            if ((num >= 100) && (num <= 200)) //&& significa "e", || significa "ou"
+                { return num + " está entre 100 e 200!"; }
+            else
+                { return num + " não está entre 100 e 200!"; }
+        }
+
+        public string Exercicio03(int idade)
+        {
+            if ((idade >= 16) && (idade < 18) || (idade > 70))
+            { return "Voto facultativo, você pode votar! "; }
+
+            else if (idade>=18) 
+                { return "Voto obrigatório!"; }
+            else { return "Não pode votar ainda!"; }
+        }
+
+
+        public string Exercicio04(int interv1, int interv2, int numComp)
+        {
+            if ((numComp >= interv1) && (numComp <= interv2))
+            { return numComp + " Está dentro do intervalo!"; }
+            else { return numComp + " Não está dentro do intervalo!"; }
+        }
+
+
+        public string Exercicio05()
+        {
+            int num1, num2;
+            bool achou = false;
+
+            Console.Write("Informe um número que deseja procurar: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Informe o valor " + (i + 1) + ": ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                if (num1 == num2)
+                {
+                    achou = true;
+                }
+            }//fim for
+
+            if (achou)
+            {
+                return "'" + num1 + "'" + " O número está entre os valores informados";
+            }
+            else
+            {
+                return "'" + num1 + "'" + " O número não está dentro";
+            }//fim if
+        }//fim do metodo Ex05
+
+    
+
+
+              
+    }//fim da classe
+}//fim do projeto
